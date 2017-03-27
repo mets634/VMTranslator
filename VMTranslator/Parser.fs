cf3c5bdbeb427cfbb55d6406fcfda58f70bb68a7
yedidya <yedidyakfir@gmail.com>
2017-03-27 18:23:06 +0300

    create regex for pop

--- VMTranslator/Parser.fs
+++ VMTranslator/Parser.fs
@@ -27,6 +27,7 @@ let Parse line =
     | ParseRegex GT_REGEX _ -> MapCommand GT_REGEX
     | ParseRegex AND_REGEX _ -> MapCommand AND_REGEX
     | ParseRegex OR_REGEX _ -> MapCommand OR_REGEX
+    | ParseRegex POP_REGEX [seg;number;_] -> String.Format(MapCommand POP_REGEX,GetSegIndex seg,number)
     | _ -> String.Format("ERROR: {0}", line)
 
 
