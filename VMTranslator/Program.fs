e922909dc13bc45a068810d32fb32867f1cec805
yonah mann <mets634@gmail.com>
2017-03-28 20:16:13 +0300

    Refactored

--- VMTranslator/Program.fs
+++ VMTranslator/Program.fs
@@ -6,26 +6,26 @@ open Parser
 open System.Text.RegularExpressions
 
 let prefix = @"
-        @256
-        D=A
-        @0
-        M=D
-        @300
-        D=A
-        @1
-        M=D
-        @400
-        D=A
-        @2
-        M=D
-        @3000
-        D=A
-        @3
-        M=D
-        @3010
-        D=A
-        @4
-        M=D"
+    @256
+    D=A
+    @0
+    M=D
+    @300
+    D=A
+    @1
+    M=D
+    @400
+    D=A
+    @2
+    M=D
+    @3000
+    D=A
+    @3
+    M=D
+    @3010
+    D=A
+    @4
+    M=D"
 
 let Translate (file:string) = 
     let singleLineCommentRegex = @"^//.*$"
