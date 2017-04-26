3552ee7cd58a771dc7af58d316fc675cd9e4b847
yedidya <yedidyakfir@gmail.com>
2017-04-26 17:09:50 +0300

    Fixed merge.

--- VMTranslator/Command2.fs
+++ VMTranslator/Command2.fs
@@ -7,13 +7,29 @@ let LABEL_REGEX = @"^label (.+)(\s*)$"
 
 [<Literal>]
 let LABEL_ASM = @"
-({0})"
+(LABEL {0})"
 
 [<Literal>]
 let GOTO_REGEX = @"^goto (.+)(\s*)$"
 
 [<Literal>]
-let GOTO_ASM = @""
+let GOTO_ASM = @"
+    @0
+    D=A
+    @{0}
+    0;JMP"
+
+[<Literal>]
+let IFGOTO_REGEX = @"^if-goto (.+)(\s*)$"
+
+[<Literal>]
+let IFGOTO_ASM = @"
+    @0
+    M=M-1
+    A=M
+    D=M
+    @{0}
+    D;JNE"
 
 [<Literal>]
 let FUNCTION_REGEX = @"^function (.+)(\s*) (\d+)(\s*)$"
